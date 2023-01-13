8ec4c31d8c6782cd738b55d2b507ff75c098d21e
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2023-01-13 17:33:13 +0530

    02 Code commit

---
+++ Adobe/02IncreasingTripletSubsequence.cpp
@@ -0,0 +1,34 @@
+// 334. Increasing Triplet Subsequence
+
+// Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+// Example 1:
+
+// Input: nums = [1,2,3,4,5]
+// Output: true
+// Explanation: Any triplet where i < j < k is valid.
+// Example 2:
+
+// Input: nums = [5,4,3,2,1]
+// Output: false
+// Explanation: No triplet exists.
+
+
+class Solution {
+public:
+  bool increasingTriplet(vector<int>& nums) {
+
+int c1 = INT_MAX, c2 = INT_MAX;
+    for (int x : nums) {
+        if (x <= c1) {
+            c1 = x;           
+        } else if (x <= c2) { // here when x > c1, i.e. x might be either c2 or c3
+            c2 = x;           // x is better than the current c2, store it
+        } else {              // here when we have/had c1 < c2 already and x > c2
+            return true;      // the increasing subsequence of 3 elements exists
+        }
+    }
+    return false;
+
+      
+    }
+};
