435d9ac222c66bcd62833feb32b6b5468892f7aa
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2023-01-24 16:10:24 +0000

    01 Code commit

--- Google/01MaximumConsecutiveFloors.cpp
+++ Google/01MaximumConsecutiveFloors.cpp
@@ -2,7 +2,6 @@
 // Alice manages a company and has rented some floors of a building as office space. Alice has decided some of these floors should be special floors, used for relaxation only.
 
 // You are given two integers bottom and top, which denote that Alice has rented all the floors from bottom to top (inclusive). You are also given the integer array special, where special[i] denotes a special floor that Alice has designated for relaxation.
-
 // Return the maximum number of consecutive floors without a special floor.
 // Example 1:
 
