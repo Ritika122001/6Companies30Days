beae212c58349d105ee0fc40296263dc56411a61
Ritika Khanduri <56202980+Ritika122001@users.noreply.github.com>
2023-01-30 17:41:24 +0530

    Create 13TopKFrequentWords.cpp

---
+++ Amazon/13TopKFrequentWords.cpp
@@ -0,0 +1,47 @@
+// 692. Top K Frequent Words
+// Medium
+// 6.6K
+// 312
+// Companies
+// Given an array of strings words and an integer k, return the k most frequent strings.
+// Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
+// Example 1:
+// Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+// Output: ["i","love"]
+// Explanation: "i" and "love" are the two most frequent words.
+// Note that "i" comes before "love" due to a lower alphabetical order.
+// Example 2:
+
+// Input: words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4
+// Output: ["the","is","sunny","day"]
+// Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.
+
+struct Compare {
+    bool operator() (pair<int, string> a, pair<int, string> b) {
+        if(a.first == b.first)
+            return a.second > b.second;
+        else
+            return a.first < b.first;
+    }
+};
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> m;
+        for(int i=0; i<words.size(); i++)
+            m[words[i]]++;
+        
+        priority_queue<pair<int, string>, vector<pair<int, string>>, Compare> q;
+        for(auto p : m)
+            q.push({p.second, p.first});
+        
+        vector<string> ans;
+        while(k--) {
+            ans.push_back(q.top().second);
+            q.pop();
+        }
+        
+        return ans;
+    }
+};
